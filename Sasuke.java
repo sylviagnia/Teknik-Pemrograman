2053f781339db08182ec7d11071718dadf35d7ca
sylviagnia <agniasylvia3@gmail.com>
2021-04-18 22:19:38 +0700

    tugas3
    naruto

---
+++ Sasuke.java
@@ -0,0 +1,11 @@
+package Naruto;
+
+public class Sasuke extends Itachi {
+	String Dojutsu = "Sharingan";
+
+	public void printDojutsu() {
+		// TODO Auto-generated method stub
+		super.printDojutsu();
+		System.out.println(this.Dojutsu);
+	}
+}
